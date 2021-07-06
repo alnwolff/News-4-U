ab22e84e54232faf73f3468e3516c5b7070a39dd
Raoul Ullens <raoulullens@live.com>
2021-07-06 16:45:06 +0200

    add first version of article list

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,13 @@ document.addEventListener(
   },
   false
 );
+
+
+// axios.get(`https://newsapi.org/v2/everything?q=Apple&from=2021-07-06&sortBy=popularity&apiKey=55a7e090588c43a8b6202dcd3c96e2ed`)
+// 		.then(response => {
+// 			console.log(response);
+// 		})
+// 		.catch(err => {
+// 			console.log(err);
+// 		})
+
--- routes/articleOverview.js
+++ routes/articleOverview.js
@@ -1,9 +1,28 @@
 const router = require("express").Router();
 
+// import News API
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('55a7e090588c43a8b6202dcd3c96e2ed');
+
 /* GET articleOverview page */
 router.get("/articleoverview", (req, res, next) => {
-  res.render("articleOverview/articleOverview.hbs");
+  newsapi.v2.everything({
+    q: 'bitcoin',
+    sources: 'bbc-news,the-verge',
+    domains: 'bbc.co.uk, techcrunch.com',
+    language: 'en',
+    sortBy: 'relevancy',
+    page: 2
+    }).then(articleList => {
+    console.log("this is the console log");
+    // console.log(articleList.articles[0]);
+    res.render("articleOverview/articleOverview.hbs", {article: articleList.articles});
+    });
 });
+
+
+
+
 
 
 module.exports = router;
