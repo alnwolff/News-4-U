18f69f01e86684e9530b5467fc809f36738a3853
Raoul Ullens <raoulullens@live.com>
2021-07-06 17:22:57 +0200

    add full article overview

--- routes/articleOverview.js
+++ routes/articleOverview.js
@@ -7,9 +7,11 @@ const newsapi = new NewsAPI('55a7e090588c43a8b6202dcd3c96e2ed');
 /* GET articleOverview page */
 router.get("/articleoverview", (req, res, next) => {
   newsapi.v2.everything({
-    q: 'bitcoin',
-    sources: 'bbc-news,the-verge',
-    domains: 'bbc.co.uk, techcrunch.com',
+    // q: 'bitcoin',
+    // to: Date.now(),
+    // Add id of sources
+    sources:"bbc-news, the-wall-street-journal, google-news, cnn, cbs-news, techcrunch",
+    from: '2021-07-04',
     language: 'en',
     sortBy: 'relevancy',
     page: 2
