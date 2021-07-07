e730a67abfab3186a133b86f6c2e45167e9e3ebf
Alan Sanchez <alnwolff@gmail.com>
2021-07-07 15:48:05 +0200

    add filter with user interests function

--- routes/articleOverview.js
+++ routes/articleOverview.js
@@ -12,20 +12,40 @@ const newsapi = new NewsAPI('55a7e090588c43a8b6202dcd3c96e2ed');
 
 /* GET articleOverview page */
 router.get("/articleoverview", (req, res, next) => {
-  newsapi.v2.everything({
-    // q: 'bitcoin',
-    // to: Date.now(),
-    // Add id of sources
-    sources:"bbc-news, the-wall-street-journal, google-news, cnn, cbs-news, techcrunch",
-    from: "2021-07-05",
-    language: 'en',
-    sortBy: 'relevancy',
-    page: 2
-    }).then(articleList => {
-    console.log(articleList);
-    // console.log(articleList.articles[0]);
-    res.render("articleOverview/articleOverview.hbs", {article: articleList.articles});
-    });
+  User.findById(req.session.user._id)
+    .then(userFromDB => {
+
+      function addOperator(array) {
+        let interests = ''
+        for(let i = 0; i < array.length ; i ++) {
+         if (i < array.length -1) {
+           interests += array[i] + ' OR '
+         } else {
+           interests += array[i]
+         }
+        }
+        return (interests)
+      }
+      console.log(addOperator(userFromDB.interests));
+
+      newsapi.v2.everything({
+        q: addOperator(userFromDB.interests),
+        // to: Date.now(),
+        // Add id of sources
+        sources:"bbc-news, the-wall-street-journal, google-news, cnn, cbs-news, techcrunch",
+        from: "2021-07-05",
+        language: 'en',
+        sortBy: 'relevancy',
+        page: 2
+        }).then(articleList => {
+        console.log(articleList);
+        res.render("articleOverview/articleOverview.hbs", {article: articleList.articles});
+        });
+    })
+
+    .catch((err) => {
+      next(err);
+    })
 });
 
 
