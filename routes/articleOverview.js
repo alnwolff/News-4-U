fb175c125fce884e3a5176dcd5317e6c14cad8b1
Alan Sanchez <alnwolff@gmail.com>
2021-07-07 15:03:18 +0200

    add read later section in profile view

--- routes/articleOverview.js
+++ routes/articleOverview.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const User = require('../models/User');
 
 // import News API
 const NewsAPI = require('newsapi');
@@ -6,24 +7,34 @@ const newsapi = new NewsAPI('55a7e090588c43a8b6202dcd3c96e2ed');
 
 /* GET articleOverview page */
 router.get("/articleoverview", (req, res, next) => {
-  newsapi.v2.everything({
-    // q: 'bitcoin',
-    // to: Date.now(),
-    // Add id of sources
-    sources:"bbc-news, the-wall-street-journal, google-news, cnn, cbs-news, techcrunch",
-    from: '2021-07-04',
-    language: 'en',
-    sortBy: 'relevancy',
-    page: 2
-    }).then(articleList => {
-    console.log("this is the console log");
-    // console.log(articleList.articles[0]);
-    res.render("articleOverview/articleOverview.hbs", {article: articleList.articles});
-    });
+  User.findById(req.session.user._id)
+    .then(userFromDB => {
+
+      // console.log(userFromDB.interests);
+      newsapi.v2.everything({
+        q: "music OR football OR finance",
+        // sources:"bbc-news, the-wall-street-journal, google-news, cnn, cbs-news, techcrunch",
+        from: '2021-07-04',
+        language: 'en',
+        sortBy: 'relevancy',
+        page: 1
+        })
+          .then(articleList => {
+          // Article.create
+          console.log("this is the console log");
+          console.log(articleList);
+          res.render("articleOverview/articleOverview.hbs", {article: articleList.articles});
+          });
+    })
+
+    .catch((err) => {
+      next(err);
+    })
 });
 
-
-
+router.post('/articleoverview', (req, res, next) => {
+  console.log(req.body)
+})
 
 
 
