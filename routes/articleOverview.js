bfbc81e8d2979df62a063d54abe4440b8c77e2be
Raoul Ullens <raoulullens@live.com>
2021-07-07 14:53:05 +0200

    add html structure of newsfeed

--- routes/articleOverview.js
+++ routes/articleOverview.js
@@ -4,6 +4,12 @@ const router = require("express").Router();
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('55a7e090588c43a8b6202dcd3c96e2ed');
 
+// var todayTimeStamp = new Date(); 
+// var oneDayTimeStamp = 1000 * 60 * 60 * 24; // Milliseconds in a day
+// var diff = todayTimeStamp - oneDayTimeStamp;
+// var yesterdayDate = new Date(diff);
+
+
 /* GET articleOverview page */
 router.get("/articleoverview", (req, res, next) => {
   newsapi.v2.everything({
@@ -11,12 +17,12 @@ router.get("/articleoverview", (req, res, next) => {
     // to: Date.now(),
     // Add id of sources
     sources:"bbc-news, the-wall-street-journal, google-news, cnn, cbs-news, techcrunch",
-    from: '2021-07-04',
+    from: "2021-07-05",
     language: 'en',
     sortBy: 'relevancy',
     page: 2
     }).then(articleList => {
-    console.log("this is the console log");
+    console.log(articleList);
     // console.log(articleList.articles[0]);
     res.render("articleOverview/articleOverview.hbs", {article: articleList.articles});
     });
